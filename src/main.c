0da7e97efe5b22f0c9453aab8f5a622744ad5329
redpois0n <red@cmail.nu>
2015-11-01 00:10:38 +0100

    get clipboard content function

--- src/main.c
+++ src/main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include <windows.h>
+
+char *get_clipboard_content(void) {
+	OpenClipboard(NULL);
+	HANDLE data = GetClipboardData(CF_TEXT);
+	GlobalLock(data);
+	char *c = (char*) data;
+	GlobalUnlock(data);
+
+	return c;
+}
 
 int main(void) {
-
 	return 0;
 }
